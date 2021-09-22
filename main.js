0f00b73f51e505b37b8f3a5304965b8767bf43de
youhavetosleep <qksekf4@gmail.com>
2021-09-22 15:56:08 +0900

    detail done

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
     let preScrollHeight = 0 // 현재 스크롤 위치(yOffet)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
     let currentScene = 0 // 현재 활성화된 (눈 앞에 보고 있는) 씬 (scroll- section)
     let enterNewScene = false; // 새로운  scene이 시작되는 순간 true
-    let acc = 0.1 ; // 가속도 곱하는 변수 
+    let acc = 0.2 ; // 가속도 곱하는 변수 
     let delayedYOffset = 0;
     let rafId;
     let rafState; 
@@ -504,10 +504,17 @@
 
             preScrollHeight += sceneInfo[i].scrollHeight;
         }
-        
+        if(delayedYOffset < preScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            document.body.classList.remove('scroll-effect-end')
+        }
         if(delayedYOffset > preScrollHeight + sceneInfo[currentScene].scrollHeight) {
             enterNewScene =true;
-            currentScene++;
+            if(currentScene < sceneInfo.length -1){
+                currentScene++;
+            }
+            if(currentScene === sceneInfo.length -1){
+                document.body.classList.add('scroll-effect-end');
+            }
             document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
         if(delayedYOffset < preScrollHeight) {
@@ -534,7 +541,18 @@
                 }
             }
         }
-
+        // 일부 기기에서 페이지 끝으로 고속 이동하면 body id가 제대로 인식 안되는 경우를 해결
+        // 페이지 맨 위로 갈 경우: scrollLoop와 첫 scene의 기본 캔버스 그리기 수행
+        if (delayedYOffset < 1) {
+            scrollLoop();
+            sceneInfo[0].objs.canvas.style.opacity = 1;
+            sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
+        }
+        // 페이지 맨 아래로 갈 경우: 마지막 섹션은 스크롤 계산으로 위치 및 크기를 결정해야할 요소들이 많아서 1픽셀을 움직여주는 것으로 해결
+        if ((document.body.offsetHeight - window.innerHeight) - delayedYOffset < 1) {
+            let tempYOffset = yOffset;
+            scrollTo(0, tempYOffset - 1);
+        }
 
         rafId = requestAnimationFrame(loop); 
 
@@ -548,39 +566,70 @@
 
 
 
-    window.addEventListener('scroll', ()=> {
-        yOffset = window.pageYOffset // 현재 스크롤 값 출력 
-        scrollLoop(); 
-        checkMenu();
 
-        if(!rafState) {
-            rafId = requestAnimationFrame(loop);
-            rafState = true;
-        }
-
-
-    });
     window.addEventListener('load', ()=> {
+        setLayout(); // 중간에 새로고침 시, 콘텐츠 양에 따라 높이 계산에 오차가 발생하는 경우를 방지하기 위해 before-load 클래스 제거 전에도 확실하게 높이를 세팅하도록 한번 더 실행
+        //'before-load' 로딩표시 때문
         document.body.classList.remove('before-load');
-        
         setLayout();
         sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0); 
-    });
-    // window.addEventListener('DOMContentLoad', setLayout); 위와 같은 의미 
-    window.addEventListener('resize', ()=> {
-        if(window.innerWidth > 900) {
-            setLayout();
+
+
+        let tempYOffset = yOffset;
+        let tempScrollCount = 0;
+        if( tempYOffset > 0 ) {
+            // setInterval은 반복적으로 로드하기 위해 사용 
+            // setTimeout은 1번만 실행 
+            let siId = setInterval(() => {
+                //스크롤을 살짝 넣어줌 (scrollTo)
+                scrollTo(0, tempYOffset);
+                tempYOffset += 5;
+                
+                if(tempScrollCount > 20) {
+                    //20이 넘어갈때 멈추기 위해서 clearInterval 시용
+                    //requestAnimationFrame과 비슷한 역할 
+                    clearInterval(siId);
+                }
+                tempScrollCount++;
+            }, 20);
+            // window.scrollTo(0,2)
         }
-        // 창 리사이즈시 레이아웃이 안맞는 현상에 대처하기 위해 
-        // 리사이즈 될 때마다 rectStartY을 초기화 함 
-        sceneInfo[3].values.rectStartY = 0;
+
+        window.addEventListener('scroll', ()=> {
+            yOffset = window.pageYOffset // 현재 스크롤 값 출력 
+            scrollLoop(); 
+            checkMenu();
+    
+            if(!rafState) {
+                rafId = requestAnimationFrame(loop);
+                rafState = true;
+            }
+        });
+        // window.addEventListener('DOMContentLoad', setLayout); 위와 같은 의미 
+        window.addEventListener('resize', ()=> {
+            if(window.innerWidth > 900) {
+                window.location.reload();
+                // setLayout();
+                // // 창 리사이즈시 레이아웃이 안맞는 현상에 대처하기 위해 
+                // // 리사이즈 될 때마다 rectStartY을 초기화 함 
+                // sceneInfo[3].values.rectStartY = 0;
+            }
+        });
+
+        //orientationchange 모바일 가로 세로 변경시에 일어나는 이벤트 
+        window.addEventListener('orientationchange', ()=> {
+            scrollTo(0, 0);
+            setTimeout(()=> {
+                window.location.reload();
+            }, 500);
+        });
+
+        //transitionend은 트렌지션이 끝나고 실행되는 메소드 
+        document.querySelector('.loading').addEventListener('transitionend', (e) => {
+            document.body.removeChild(e.currentTarget)
+        })
     });
-    //orientationchange 모바일 가로 세로 변경시에 일어나는 이벤트 
-    window.addEventListener('orientationchange', setLayout);
-    //transitionend은 트렌지션이 끝나고 실행되는 메소드 
-    document.querySelector('.loading').addEventListener('transitionend', (e) => {
-        document.body.removeChild(e.currentTarget);
-    })
+
 
     setCanvasImages();
 
